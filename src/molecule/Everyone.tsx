e9a9cd334a4a2f6113a61bf9884cbf9f9d9f1f1b
MUKEBUDAVID <davidmukebu@gmail.com>
2024-06-21 12:43:05 +0100

    fin du responsive de everyone du main

--- src/molecule/Everyone.tsx
+++ src/molecule/Everyone.tsx
@@ -1,4 +1,6 @@
-// import React from 'react'
+// import React from 'react';
+import illustration1 from "../assets/bg-tablet-pattern.svg";
+import illustration2 from "../assets/illustration-intro.svg";
 
 import GetButon from "../atom/GetButon";
 
@@ -9,13 +11,21 @@ export default function Everyone() {
         <h1>
         Bring everyone together to build better products.
         </h1> 
+        <p>
+        Manage makes it simple for software teams to plan day-to-day 
+        tasks while keeping the larger team goals in view.
+        </p>
 
         <GetButon decoration="everyoneBtn"/>
 
       </div>
 
 
-      <div className="right"></div>
+      <div className="right">
+        <img src={illustration1} alt="illustration1"  />
+     <img src={illustration2} alt="illustration2"  />
+
+      </div>
     </section>
   )
 }
