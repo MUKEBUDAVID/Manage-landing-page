94d79d2b1e5f961a5aa709557cd4cc7759809923
MUKEBUDAVID <davidmukebu@gmail.com>
2024-06-22 10:10:07 +0100

    fin de la partie left de la section manage pour descktop

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,10 +1,13 @@
 // import React from "react";
+import Manage from "../atom/Manage"
 import Everyone from "../molecule/Everyone"
 
 export default function Main() {
   return (
     <main>
- <Everyone/>        
+ <Everyone/> 
+  
+ <Manage/>      
 
     </main>
   )
