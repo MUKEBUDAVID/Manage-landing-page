af0dd2cfc75c7564ae4f5074219a7521993093bf
MUKEBUDAVID <davidmukebu@gmail.com>
2024-08-18 09:39:48 +0100

    fin du responsive la partie tablet

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,114 @@
-// import React from 'react'
 
+import { FormEvent,ChangeEvent,useState } from "react";
+import isEmailValid from "../util/IsEmailValid";
 export default function Footer() {
+
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+
+
+  
+
+
+  const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+   setInputValue(value);
+
+   inputValue===""?setError(""):null
+
+   
+
+};
+
+
+
+
+const  handleSubmitEmail=(e:FormEvent)=>{
+  e.preventDefault();
+  e.stopPropagation();
+
+if (inputValue!=="") {
+  
+  if (!isEmailValid(inputValue)) {
+    setError('please insert a valid email');
+  }else{
+    setError('');
+  }
+  
+}
+
+   
+  
+
+}
+
+
+
+
+
+
+
+
+
   return (
-    <div>Footer</div>
+    <footer>
+
+        <section className="contact">
+          <img src="/src/assets/logoFooter.svg" alt="logo" className="logo" />
+          
+          <div className="sociaux"  >
+ <a href="#"><img src="/src/assets/icon-facebook.svg" alt="icon-facebook"/></a>
+ <a href="#"><img src="/src/assets/icon-youtube.svg" alt="icon-youtube" /></a>
+ <a href="#"><img src="/src/assets/icon-twitter.svg" alt="icon-twitter"/></a>
+ <a href="#"><img src="/src/assets/icon-pinterest.svg" alt="icon-pinterest" /></a>
+ <a href="#"><img src="/src/assets/icon-instagram.svg" alt="icon-instagram.svg" /></a>
+</div>
+        </section>
+        
+
+
+<section className="navigation">
+
+<nav>
+ <a href="#">Home</a>   
+ <a href="#">Pricing</a>   
+ <a href="#">Products</a>   
+ <a href="#">About Us</a>   
+</nav>
+
+
+<nav>
+ <a href="#">Careers</a>   
+ <a href="#">Community</a>   
+ <a href="#">Privacy Policy</a>   
+</nav>
+
+</section>
+
+
+
+<section className="newslater">
+
+  <form action="" method="post" onSubmit={handleSubmitEmail}>
+  <input type="email" name="email" onChange={handleInputChange} value={inputValue} id="email" placeholder="Updates in your inbox…" />
+  <button type="submit">Go</button>
+   <span >{error}</span>
+
+  </form>
+
+
+  <p>Copyright 2020. All Rights Reserved</p>
+
+
+</section>
+
+
+<div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">David Mukebu</a>.
+  </div>
+
+
+    </footer>
   )
 }
